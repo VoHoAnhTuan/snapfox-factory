e4019a0254bde75bd261e823ba5059caf33a1c6a
Tuan Vo <hoanhtuan.vo@gmail.com>
2026-02-03 01:07:36 +0700

    feat: render inspirational quote

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,19 @@
 import { Heading, Text, VStack } from "@chakra-ui/react";
-
+import { useState, useEffect } from 'react';
+import Quote from 'inspirational-quotes';
 export default function Home() {
+  // inpirational quote
+  const [quote, setQuote] = useState({text: ""});
+
+  useEffect(() => {
+    const q = Quote.getQuote({author: true});
+    setQuote(q);
+  }, []);
+
   return (
-    <VStack align="start" gap="4">
-      <Heading size="2xl" color="fg">Dashboard Overview</Heading>
-      <Text color="fg">Welcome back! Here is what's happening today.</Text>
+    <VStack color="fg" align="start" gap="2">
+      <Heading size="2xl" >Dashboard Overview</Heading>
+      <Text fontSize="sm" fontStyle="italic" color="fg.muted">{quote.text} _ {quote.author} _</Text>
     </VStack>
   );
 }
